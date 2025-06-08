3e7e0efd10164c323fd2ca22318c18ee3ddf85fb
Yule Lima <yulehohenfeld@gmail.com>
2025-06-08 15:38:09 -0300

    chore: Adiciona comentário de teste

--- services/api.ts
+++ services/api.ts
@@ -1,13 +1,11 @@
-// Conteúdo do arquivo: src/services/api.ts
 
 import axios from 'axios';
 
-// O endereço do seu servidor.
-// Se estiver testando no emulador Android, use 'http://10.0.2.2:8007'
-// Para iOS ou web, 'http://localhost:8007' funciona.
 const api = axios.create({
 baseURL: 'http://192.168.0.66:8007'
 
 });
 
 export default api;
+
+// Testando uma nova atualização para o Git.
